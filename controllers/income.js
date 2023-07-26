5bb361b57be578a6511c84afc71ca419b96c6c09
ycuevas <ycuevas@isf.do>
2023-07-26 15:36:23 -0400

    token validation in incomes and users

--- controllers/income.js
+++ controllers/income.js
@@ -8,7 +8,6 @@ exports.addIncome = async (req, res) => {
 
     const authHeader = req.headers.authorization;
     const token = authHeader.split(" ")[1];
-
     const { title, amount, category, description, date } = req.body
     const income = IncomeSchema({
         title,
@@ -18,20 +17,30 @@ exports.addIncome = async (req, res) => {
         date
     })
 
-    try {
-        if (!title || !amount || !category || !description || !date) {
-            return res.status(400).json({ message: 'All fields are required' })
-        }
-        if (amount <= 0 || !amount === 'number') {
-            return res.status(400).json({ message: 'Amount must ve a positive number' })
-        }
+    jsonwebtoken.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (decoded) {
+            try {
+                if (!title || !amount || !category || !description || !date) {
+                    return res.status(400).json({ message: 'All fields are required' })
+                }
+                if (amount <= 0 || !amount === 'number') {
+                    return res.status(400).json({ message: 'Amount must be a positive number' })
+                }
 
-        income.save()
-        res.status(200).json({ Message: 'Income Created', income })
-    } catch (error) {
+                income.save()
+                res.status(200).json({ Message: 'Income Created', income })
+            } catch (error) {
 
-        res.status(500).json({ Message: 'Server Error' })
-    }
+                res.status(500).json({ Message: 'Server Error' })
+            }
+        } else {
+            res.status(401).json(err.message)
+        }
+    })
+
+
+
+
 }
 
 exports.getIncomes = async (req, res) => {
@@ -42,21 +51,46 @@ exports.getIncomes = async (req, res) => {
     const authHeader = req.headers.authorization;
     const token = authHeader.split(" ")[1];
 
-    try {
-        const incomes = await IncomeSchema.find({ status: true }).sort({ createdAt: -1 })
-        res.status(200).json(incomes)
-    } catch (err) {
-        res.status(500).json({ Message: 'Server Error', error: err })
-    }
+    jsonwebtoken.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (decoded) {
+            try {
+                const incomes = await IncomeSchema.find({ status: true }).sort({ createdAt: -1 })
+                res.status(200).json(incomes)
+            } catch (err) {
+                res.status(500).json({ Message: 'Server Error', error: err })
+            }
+        }
+
+        else {
+            res.status(401).json(err.message)
+        }
+    })
+
 
 }
 
 exports.deleteIncome = async (req, res) => {
 
+    if (!req.headers.authorization) {
+        return res.status(401).json({ error: "Not Authorized" });
+    }
+
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
     const { id } = req.params;
-    IncomeSchema.findByIdAndUpdate(id, { status: false }).then((result) => {
-        res.status(200).json({ Message: 'Income Deleted', result })
-    }).catch((err) => {
-        res.status(500).json({ Message: 'Server Error' })
-    });
+
+    jsonwebtoken.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (decoded) {
+
+            IncomeSchema.findByIdAndUpdate(id, { status: false }).then((result) => {
+                res.status(200).json({ Message: 'Income Deleted', result })
+            }).catch((err) => {
+                res.status(500).json({ Message: 'Server Error' })
+            });
+        }
+        else {
+            res.status(401).json(err.message)
+        }
+    })
+
 }
